3fba93b74fe8db51431c08c4379beba0a6d0d0d8
smr.hhs.laptop <robinolotje@gmail.com>
2021-04-07 12:10:37 +0200

    adjusting kernels and make camera operate continuously

--- RailDetection/RailCamera.py
+++ RailDetection/RailCamera.py
@@ -1,29 +1,27 @@
 import cv2
 import numpy as np
 
+TEST = False
+
 
 def find_paste(image):
     image_copy = image.copy()
+    image_gray = cv2.cvtColor(image_copy, cv2.COLOR_BGR2GRAY)
+    image_height, image_width = image_gray.shape
 
-    image_height, image_width = image_copy.shape
+    crop_away_top = int(image_height / 3) + 50
+    crop_away_bottom = int(image_height / 3) - 50
+    image_cropped = image_gray[crop_away_top: image_height - crop_away_bottom, 0: image_width]
+    image_cropped_black_and_white = cv2.threshold(image_cropped, 125, 255, cv2.THRESH_BINARY)[1]
 
-    height_to_crop_away = int(image_height / 3)
+    kernel = np.ones((2, 4), np.uint8)
+    erosion = cv2.erode(image_cropped_black_and_white, kernel, iterations=4)
+    kernel = np.ones((10, 4), np.uint8)
+    dilation = cv2.dilate(erosion, kernel, iterations=20)
 
-    image_cropped = image_copy[height_to_crop_away: image_height - height_to_crop_away, 0: image_width]
+    contours, _ = cv2.findContours(dilation.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    image_cropped_black_and_white = cv2.threshold(image_cropped, 128, 255, cv2.THRESH_BINARY)[1]
-
-    iterations = 10
-    kernel = np.ones((6, 6), np.uint8)
-    erosion = cv2.erode(image_cropped_black_and_white, kernel, iterations=iterations)
-    kernel = np.ones((5, 5), np.uint8)
-    dilation = cv2.dilate(erosion, kernel, iterations=iterations)
-
-    edged = cv2.Canny(dilation, 30, 200)
-
-    contours, _ = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    output_image = cv2.cvtColor(image_cropped_black_and_white, cv2.COLOR_GRAY2BGR)
+    output_image = cv2.cvtColor(dilation, cv2.COLOR_GRAY2BGR)
 
     for contour in contours:
         cv2.drawContours(output_image, [contour], 0, (0, 0, 255), 2)
@@ -35,11 +33,18 @@ def find_paste(image):
         cv2.rectangle(output_image, (x, y), (x + w, y + h), (255, 0, 0), 5)
         cv2.circle(output_image, (centre_x, centre_y), 5, (255, 0, 0), 5)
 
+        cv2.rectangle(image_copy, (x, y+crop_away_top), (x + w, y + h + crop_away_top), (255, 0, 0), 5)
+        cv2.circle(image_copy, (centre_x, centre_y + crop_away_top), 5, (255, 0, 0), 5)
+
         perimeter = cv2.arcLength(contour, True)
 
         print("Perimeter: {}".format(perimeter))
         print("Coordinates are ({};{})".format(centre_x, centre_y))
-    return output_image
+
+    if TEST:
+        return output_image, image_cropped_black_and_white, erosion
+    else:
+        return output_image, image_copy
 
 
 class RailCamera:
@@ -47,24 +52,28 @@ class RailCamera:
         # todo: determine what kind of camera to use and do the setup here, likely to just be a normal camera but
         #  we're not sure yet
         print("Initialising rail camera.")
-        user_input = input("Photo (p) or camera (c)")
-        if user_input == 'c':
-            self.cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
-            ret, self.frame = self.cap.read()
-        elif user_input == 'p':
-            self.frame = cv2.imread(r'C:\Users\smr.hhs.laptop\Downloads\Pasta 2.jpg')
-
-        self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        self.user_input = input("Photo (p) or camera (c)")
 
 
 if __name__ == "__main__":
+    # if TEST:
+    #     output_image, threshold, erosion = find_paste(RC.frame)
+    # else:
     RC = RailCamera()
-    #output_image = find_paste(RC.frame)
+    RC.cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+    brightness = 255  # min: 0, max: 255, increment:5
+    RC.cap.set(cv2.CAP_PROP_BRIGHTNESS, focus)
 
     while True:
+        #if RC.user_input == 'c':
         ret, RC.frame = RC.cap.read()
-        cv2.imshow('rail', RC.frame)
+        #elif RC.user_input == 'p':
+        #    RC.frame = cv2.imread(r'C:\Users\smr.hhs.laptop\Downloads\Pasta 2.jpg')
 
+        output_image, copy = find_paste(RC.frame)
+        cv2.imshow('rail', output_image)
+        cv2.imshow('normal', copy)
         key = cv2.waitKey(1)
 
         if key == ord('k'):
