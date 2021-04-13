142a0608ce7cb06b6e48b9e7177ae18c5ba91473
smr.hhs.laptop <robinolotje@gmail.com>
2021-04-13 14:37:54 +0200

    Finished the script by implementing area selection and sending triggers at right time. Also put in comments

--- RailDetection/RailCamera.py
+++ RailDetection/RailCamera.py
@@ -1,9 +1,6 @@
 import cv2
 import numpy as np
 import Blob
-import socket
-import struct
-import sys
 
 TEST = False
 
@@ -13,8 +10,8 @@ HALF_SCREEN = 320
 old_trigger = 99
 
 # PLC configuration
-TCP_IP = '192.168.0.1' # IP address of the PLC
-TCP_PORT = 2000	# Port configured in the PLC
+TCP_IP = '192.168.0.1'  # IP address of the PLC
+TCP_PORT = 2000  # Port configured in the PLC
 
 # Camera calibration property-strings
 properties = ["CAP_PROP_FRAME_WIDTH",  # Width of the frames in the video stream.
@@ -25,61 +22,77 @@ properties = ["CAP_PROP_FRAME_WIDTH",  # Width of the frames in the video stream
               "CAP_PROP_GAIN",  # Gain of the image (only for cameras).
               "CAP_PROP_EXPOSURE"]
 
+
 def find_paste(image):
+    """This function is a tool to distill the required blobs out of an image/frame"""
+
+    # Get height and width from the grayscale image
     image_copy = image.copy()
     image_gray = cv2.cvtColor(image_copy, cv2.COLOR_BGR2GRAY)
     image_height, image_width = image_gray.shape
 
+    # Cut away unnecessary parts of the image
     crop_away_top = int(image_height / 3) + 50
     crop_away_bottom = int(image_height / 3) - 50
     image_cropped = image_gray[crop_away_top: image_height - crop_away_bottom, 0: image_width]
+
+    # Convert to a binary threshold image
     image_cropped_black_and_white = cv2.threshold(image_cropped, 120, 255, cv2.THRESH_BINARY)[1]
 
+    # Delete noise from image
     kernel = np.ones((3, 5), np.uint8)
     erosion = cv2.erode(image_cropped_black_and_white, kernel, iterations=4)
     kernel = np.ones((9, 3), np.uint8)
     dilation = cv2.dilate(erosion, kernel, iterations=10)
 
+    # Find the contour of denoised image and draw it
     contours, _ = cv2.findContours(dilation.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
     output_image = cv2.cvtColor(dilation, cv2.COLOR_GRAY2BGR)
 
+    # Interpret the contours
     for contour in contours:
         cv2.drawContours(output_image, [contour], 0, (0, 0, 255), 2)
 
-        x, y, w, h = cv2.boundingRect(contour)
-        centre_x = int(x + (w / 2))
-        centre_y = int(y + (h / 2))
+        # Select only big contours
+        if cv2.contourArea(contour) > 55000:
 
-        if RC.paste is None:
-            RC.paste = Blob.Blob(h, w, x, y, centre_x, centre_y)
+            # Draw a bounding rectangle and retreive position information
+            x, y, w, h = cv2.boundingRect(contour)
+            centre_x = int(x + (w / 2))
+            centre_y = int(y + (h / 2))
 
-        elif abs(centre_x - RC.paste.centre_x) < 10:
-            RC.paste.x = x
-            RC.paste.y = y
-            RC.paste.centre_x = centre_x
-            RC.paste.centre_y = centre_y
-            RC.paste.width = w
-            RC.paste.height = h
+            # Create a paste object from contour
+            if RC.paste is None:
+                RC.paste = Blob.Blob(h, w, x, y, centre_x, centre_y)
 
-            if RC.paste.centre_x < HALF_SCREEN:
-                RC.paste.state = True
-                cv2.rectangle(image_copy, (RC.paste.x, RC.paste.y + crop_away_top),(RC.paste.x + RC.paste.width, RC.paste.y  + RC.paste.height + crop_away_top), (0, 0, 255), 3)
-                cv2.circle(image_copy, (RC.paste.centre_x, RC.paste.centre_y + crop_away_top), 5, (0, 0, 255), 3)
+            # Update object
+            elif abs(centre_x - RC.paste.centre_x) < 10:
+                RC.paste.x = x
+                RC.paste.y = y
+                RC.paste.centre_x = centre_x
+                RC.paste.centre_y = centre_y
+                RC.paste.width = w
+                RC.paste.height = h
 
-            else:
-                cv2.rectangle(image_copy, (RC.paste.x, RC.paste.y + crop_away_top),(RC.paste.x + RC.paste.width, RC.paste.y + RC.paste.height + crop_away_top), (255, 0, 0),3)
-                cv2.circle(image_copy, (RC.paste.centre_x, RC.paste.centre_y + crop_away_top), 5, (255, 0, 0), 3)
+                # Draw red rectangle
+                if RC.paste.centre_x < HALF_SCREEN:
+                    RC.paste.state = True
+                    cv2.rectangle(image_copy, (RC.paste.x, RC.paste.y + crop_away_top),
+                                  (RC.paste.x + RC.paste.width, RC.paste.y + RC.paste.height + crop_away_top),
+                                  (0, 0, 255), 3)
+                    cv2.circle(image_copy, (RC.paste.centre_x, RC.paste.centre_y + crop_away_top), 5, (0, 0, 255), 3)
 
-        elif abs(centre_x - RC.paste.centre_x) > 10:
-            RC.paste = None
+                # Draw blue rectangle
+                else:
+                    cv2.rectangle(image_copy, (RC.paste.x, RC.paste.y + crop_away_top),
+                                  (RC.paste.x + RC.paste.width, RC.paste.y + RC.paste.height + crop_away_top),
+                                  (255, 0, 0), 3)
+                    cv2.circle(image_copy, (RC.paste.centre_x, RC.paste.centre_y + crop_away_top), 5, (255, 0, 0), 3)
 
-        if TEST:
-            perimeter = cv2.arcLength(contour, True)
-            print("Blob1")
-            print("Perimeter: {}".format(perimeter))
-            print("Width = {:.1f} ; Height = {:.1f}".format(RC.paste.width, RC.paste.height))
-
+            # If the object replaces to quick the object will be None
+            elif abs(centre_x - RC.paste.centre_x) > 10:
+                RC.paste = None
+    # No contours means No paste
     if len(contours) is None:
         RC.paste = None
 
@@ -88,26 +101,24 @@ def find_paste(image):
 
 class RailCamera:
     def __init__(self):
-        # todo: determine what kind of camera to use and do the setup here, likely to just be a normal camera but
-        #  we're not sure yet
+
         print("Initialising rail camera.")
-        self.paste = None
-        self.user_input = input("Photo (p) or camera (c)")
-        self.cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        self.paste = None                                                   # Paste object
+        self.cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)                       # Get camera
 
-        self.gain = 0
-        self.cap.set(cv2.CAP_PROP_GAIN, self.gain)
-        self.brightness = 123
-        self.cap.set(cv2.CAP_PROP_BRIGHTNESS, self.brightness)
-        self.contrast = 85
-        self.cap.set(cv2.CAP_PROP_CONTRAST, self.contrast)
-        self.saturation = 100
-        self.cap.set(cv2.CAP_PROP_SATURATION, self.saturation)
-        self.exposure = -6
-        self.cap.set(cv2.CAP_PROP_EXPOSURE, self.exposure)
+        self.gain = 0                                                       # Gain of the camera
+        self.cap.set(cv2.CAP_PROP_GAIN, self.gain)                          # Apply defined gain to camera object
+        self.brightness = 123                                               # Brightness of the camera
+        self.cap.set(cv2.CAP_PROP_BRIGHTNESS, self.brightness)              # Apply defined brightness to camera object
+        self.contrast = 85                                                  # Contrast of the camera
+        self.cap.set(cv2.CAP_PROP_CONTRAST, self.contrast)                  # Apply defined contrast to camera object
+        self.saturation = 100                                               # Saturation of the camera
+        self.cap.set(cv2.CAP_PROP_SATURATION, self.saturation)              # Apply defined saturation to camera object
+        self.exposure = -6                                                  # Exposure of the camera
+        self.cap.set(cv2.CAP_PROP_EXPOSURE, self.exposure)                  # Apply defined exposure to camera object
 
-        self.frame_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.frame_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.frame_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))      # Get frame width
+        self.frame_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))    # Get frame height
 
         print("brightness = {}".format(self.brightness))
         print("contrast = {}".format(self.contrast))
@@ -116,6 +127,7 @@ class RailCamera:
         print("frame width = {}".format(self.frame_width))
         print("frame height = {}".format(self.frame_height))
 
+    # Method to tweek camera properties when TEST == True
     def set_cap_properties(self, key):
         print("\n\n")
         for prop in properties:
@@ -149,21 +161,9 @@ class RailCamera:
 
 
 if __name__ == "__main__":
-    # if TEST:
-    #     output_image, threshold, erosion = find_paste(RC.frame)
-    # else:
-    RC = RailCamera()
 
-    # Start the connection
-    try:
-        # Try to connect to the PLC
-        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        conn.connect((TCP_IP, TCP_PORT))
-    except Exception as e:
-        # Show an error message if the script can't connect to the PLC
-        print("Error while trying to connect to the PLC. Error message: ", str(e))
-        # And stop this script
-        sys.exit()
+    RC = RailCamera()
+    trigger = 0
 
     while True:
         ret, RC.frame = RC.cap.read()
@@ -177,20 +177,19 @@ if __name__ == "__main__":
 
         if RC.paste is not None:
             if RC.paste.state:
-                if old_trigger == 99:
+
+                # Send trigger to PLC when the paste passes the camera
+                if old_trigger != 3:
                     trigger = 3
                     old_trigger = 3
+                    print("Trigger = {}".format(trigger))
 
+        else:
+            # Send reset trigger to PLC
             if old_trigger == 3:
                 trigger = 99
                 old_trigger = 99
-
-        # # Convert the number to bytes in big-endian style
-        # send_data = struct.pack(">h", trigger)
-        # # print("Raw: ", send_data)
-        #
-        # # Send the data
-        # conn.send(send_data)
+                print("Trigger = {}".format(trigger))
 
         key = cv2.waitKey(1)
 
